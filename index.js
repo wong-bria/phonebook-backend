611c86f6a1b7bd4bb0515ea0e8f9817444c498d2
wong-bria <wong.bria@northeastern.edu>
2025-08-15 18:10:49 -0400

    added a route for /

--- index.js
+++ index.js
@@ -38,6 +38,10 @@ let data = [
     }
 ]
 
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
 
 app.get('/api/persons', (request, response) => {
     response.json(data)
